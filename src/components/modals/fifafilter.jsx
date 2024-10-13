3822f80645368e471afd85c9b66b992373cfae93
Felipe <felipe.sarmiento.as@gmail.com>
2024-10-13 16:58:34 -0400

    fix typos

--- src/components/modals/fifafilter.jsx
+++ src/components/modals/fifafilter.jsx
@@ -10,7 +10,7 @@ function FifaFilter() {
         focus was on creating a more intuitive, responsive interface while ensuring that
         users can easily navigate and refine their results based on multiple criteria.
       </p>
-      <p className="title">Issues</p>
+      <p className="title">Issues I have faced</p>
       <ul>
         <li className="pb-1">
           <strong>Rarity filter:</strong> It is impossible for first time users to know
@@ -21,17 +21,13 @@ function FifaFilter() {
           cumbersome to search and find a specific moment.
         </li>
         <li className="pb-1">
-          <strong>Filter by Drop is not Inclusive:</strong> You only only which items to
-          search for the drops your participated in.
+          <strong>Drops are hard to filter by:</strong> Users won't remember what items
+          belong to what drops.
         </li>
         <li className="pb-1">
           <strong>Scope:</strong> New users cannot tell what the website is about:
-          moments, RightToBuys, posters, physical claims?.
+          moments, RightToBuys, posters, physical claims?
         </li>
-        {/* <li className="pb-1">
-          <strong>Is this working?:</strong> Mobile users cannot tell if their filter
-          selection is working while they are selecting filters.
-        </li> */}
       </ul>
 
       <div className="innerGrid" style={{ width: "100%" }}>
@@ -44,10 +40,11 @@ function FifaFilter() {
 
         <p className="title">1) Define users needs</p>
         <p className="description">
-          I have been the user of this platform for two years, spend a good amount of
-          money and even onboarded my friends to. In addition, I have quite a deep
-          knowledge of futbol federations, clubs, and FIFA to make inform decisions an
-          autoprescribing the user needs for this tasks.
+          I have define this user nedds based on my experience, deep knowledge of futbol
+          federations, clubs, and FIFA to make some inform guesses about the user pain
+          points. In addition, I have been a user of this platform for two years, spent a
+          good amount of money, hold a VIP membership and even onboarded some friends. You
+          can say this user needs came from a power user.
         </p>
         <ul>
           <li className="pb-1 pt-3">
@@ -64,7 +61,7 @@ function FifaFilter() {
           </li>
           <li className="pb-1">
             ❌ <strong>Help to complete challenges:</strong> Challenges drive a lot of
-            traffic to the marketplace, yet specific filters are none existing.
+            traffic to the marketplace, yet specific filters are non-existing.
           </li>
         </ul>
 
@@ -74,18 +71,22 @@ function FifaFilter() {
           FIFA and Futbol is a global topic that attracts the most diverse users from
           different knowledge bases, not just collectible aficionados or crypto
           enthusiasts, we need inclusive language as well as clear messaging of what this
-          website has to offer. This can be improve with some rewording, we can start by
-          renaming the Drops to Moments - which is the priority of the marketplace- any
-          other collectible that doesn't belong in the moments section should be under
-          Other Collectibles. Users will start to understand that this is a collectible
-          marketplace and the main priority are the video moments.
+          website has to offer.
+          <br />
+          <br />
+          This can be improve with some rewording, we can start by renaming the Drops to
+          Moments - which is the priority of the marketplace IMO - any other collectible
+          that doesn't belong in the Moments section should be under Other Collectibles.
+          Users will start to understand that this is a marketplace for collectibles and
+          the main priority are the video moments.
           <br />
           <br />
           Another improvement is to move away from the wording of Drops in the filter
-          mechanism, instead use the word Teams which allows users to quickly think of
-          which teams they want to look a moment for. Any other moment that doesn't not
-          fall into a team (physical claims, rewards, etc) should be outside of moments
-          and into its corresponding category under Other Collectibles.
+          mechanism, instead use the word Teams or Tournaments which allows users to
+          quickly think of which teams/tournaments they want to look a moment for. Any
+          other moment that doesn't not fall into a team (physical claims, rewards, etc)
+          should be outside of moments and into its corresponding category under Other
+          Collectibles.
           <br />
           <br />
           Lastly, lets relocate the Only Right To Buy toggle to the bottom under other
@@ -112,7 +113,7 @@ function FifaFilter() {
           </li>
           <li className="pb-1">
             ❌ <strong>Help to complete challenges:</strong> Challenges drive a lot of
-            traffic to the marketplace, yet specific filters are none existing.
+            traffic to the marketplace, yet specific filters are non-existing.
           </li>
         </ul>
 
@@ -126,17 +127,18 @@ function FifaFilter() {
           need to create a smart nesting categories based on high-to-low order.
           <br />
           <br />
-          Year-specific Tournament would be the lowest order category, nested inside
-          tournament types, which are nested under the "tournament" grantparent category.
-          Similar with teams, single teams should be the lowest order of categories, folow
-          by type of type(club or country) and geographic area of teams, which would be
-          nested under the grandparent category "Teams", thus allowing users to quickly
-          select if they want to seach by tournament or teams.
+          Tournaments can be a high-order category, nested inside Tournaments are
+          types-of-tournaments, and nested inside type-of-tournaments we find
+          Year-specific Tournaments. Similar with Teams, inside Teams we find
+          type-of-teams (club or country), and inside there would be the single teams.thus
+          allowing users to quickly select if they want to seach by tournament or teams.
         </p>
         <img className="fullwidth" src="img/year/2024/fifafilter_2.png" alt="" />
         <p className="description">
-          With this change, all tournaments and teams become accessible, allowing new
-          users to quickly catchup on what is available in the market.
+          With this change, all tournaments and teams become accessible,{" "}
+          <strong>
+            allowing new users to quickly catchup on what is available in the market.
+          </strong>
         </p>
         <ul>
           <li className="pb-1 pt-3">
@@ -153,7 +155,7 @@ function FifaFilter() {
           </li>
           <li className="pb-1">
             ❌ <strong>Help to complete challenges:</strong> Challenges drive a lot of
-            traffic to the marketplace, yet specific filters are none existing.
+            traffic to the marketplace, yet specific filters are non-existing.
           </li>
         </ul>
 
@@ -161,35 +163,35 @@ function FifaFilter() {
         <p className="description">
           The goal here is to hide all filters that are not consider high-traffic, the
           less options the user will see at a glance the faster it can find what is
-          looking for and in turn improve the experience. First lets look at the Rarity
+          looking for, and in turn improve the experience. First lets look at the Rarity
           filter category, the grant majority of items are
           <em> Common, Uncommon, Rare, Epic, Iconic</em> so it makes sense to display only
           those and hide anything else.
           <br />
           <br />
-          Now lets look at the Other Collectibles category, icons make it more intuitve
+          Now lets look at the Other Collectibles category, icons make it more intuitive
           and faster to understand what these subcategories are, so lets add some icons
           under this category.
           <br />
           <br />
-          The main categories can grow and be very long - longer than the viewport - so
+          The main categories can grow and be very long - longer than the screen size - so
           lets make all the main categories collapsable, this will improve the mobile
           experience to avoid unnecessary scrolling.
         </p>
         <img className="halfwidth" src="img/year/2024/fifafilter_3.png" alt="" />
         <p className="description">
           Now that we have collapsable categories we can have a default state that is more
-          inclusive for first time users - hiding price range and other collectibles -
-          only showcasing the rarity and moments filters category. We can also keep track
-          of user preferences so next time they come back to the app it would remember
-          which fitlers they had collapsable and which were open. Yeay for great user
-          experience!
+          cleaner and simple for first time users - hiding Price Range and Other
+          Collectibles - only showcasing the Rarity and Moments filters. We can also keep
+          track of user preferences so the app would remember which fitlers they had
+          collapsable and which were open. Yeay for great user experience!
         </p>
         <img className="halfwidth" src="img/year/2024/fifafilter_4.png" alt="" />
         <p className="description">
-          When we filter all the way to our lower filter, we can use the logos and emblems
-          of tournaments, national teams and clubs teams to help users search faster and
-          with joy.
+          When the user arrives all the way to our lower-level filter (single teams,
+          single tournaments, unique categories), we can use the logos or emblems to help
+          users search faster, since they can visually scan the page for their prefer
+          logo.
         </p>
         <img className="fullwidth" src="img/year/2024/fifafilter_5.png" alt="" />
         <ul>
@@ -207,26 +209,25 @@ function FifaFilter() {
           </li>
           <li className="pb-1">
             ❌ <strong>Help to complete challenges:</strong> Challenges drive a lot of
-            traffic to the marketplace, yet specific filters are none existing.
+            traffic to the marketplace, yet specific filters are non-existing.
           </li>
         </ul>
         <div className="labels pt-4">2.3) Help to complete challenges</div>
         <p className="description">
-          FIFA collect has challenges, which sometimes come with rewards like Right To Buy
-          tickets and other limited edition prices, one feature of the challenges is that
-          most likely to complete them you have to go into the marketplace to buy the
-          missing pieces so you can fullfill the challenge. To help users complete the
-          challenges we can create custom filters for each challenge but also read the
-          users profile to know which challenge the user has completed and which it has
-          not.
+          FIFA collect has a gamify concept called Challenges, which sometimes come with
+          rewards like Right-To-Buy tickets and other limited edition items, to complete
+          Challenges the majority of users rely on the marketplace to buy their missing
+          pieces. To help users complete the challenges we can create custom filters for
+          each challenge, and access the user progress for each challenge to know which
+          challenge the user is close to completing, and which has been completed.
         </p>
         <img className="halfwidth" src="img/year/2024/fifafilter_6.png" alt="" />
         <p className="description">
           It might seem odd to add this filter mechanism here, but challenges can be very
           competitive during a short period of time, and currently is very cumbersome and
-          time consuming to look for moments. This new feature in the filter mechanism
-          would generate faster transactions -from a UX point of view-, which in turn will
-          generate more revenue on secondary sales for FIFA Collect.
+          time consuming to look for moments. This new filter mechanism would generate
+          faster transactions -from a UX point of view-, which in turn will generate more
+          revenue on secondary sales for FIFA Collect.
         </p>
         <ul>
           <li className="pb-1 pt-3">
@@ -243,7 +244,7 @@ function FifaFilter() {
           </li>
           <li className="pb-1">
             ✅ <strong>Help to complete challenges:</strong> Challenges drive a lot of
-            traffic to the marketplace, yet specific filters are none existing.
+            traffic to the marketplace, yet specific filters are non-existing.
           </li>
         </ul>
         <p className="title">3) UI Prototype Design</p>
